c3a3306f1e036c6435f2f01f7e3bb923dbb4294b
Alexander Mattheis <almat@gmx.de>
2019-08-06 20:20:49 +0200

    Updated files_lister.sh with new functionality.

--- src/files_lister.sh
+++ src/files_lister.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 OUTPUT_NAME="FilesLister.out"
 script_name=`basename "$0"`
-ls --ignore=$script_name --ignore=$OUTPUT_NAME > $OUTPUT_NAME
+
+echo -n "Print Fileendings (Default 'false')? "
+read print_file_endings
+
+echo -n "List only files (Default 'false')? "
+read list_only_files
+
+if [[ $list_only_files = "1" ]] || [[ $list_only_files = "true" ]]; then
+	if [[ $print_file_endings = "1" ]] || [[ $print_file_endings = "true" ]]; then
+		ls -p --ignore=$script_name --ignore=$OUTPUT_NAME | grep --invert-match / > $OUTPUT_NAME  # '-p' appends a slash at the front of all directories 
+	else
+		ls -p --ignore=$script_name --ignore=$OUTPUT_NAME | grep --invert-match / | sed 's/\.[a-z]*//g' > $OUTPUT_NAME  # grep to remove all with a slash at the front
+	fi 
+else
+	if [[ $print_file_endings = "1" ]] || [[ $print_file_endings = "true" ]]; then
+		ls --ignore=$script_name --ignore=$OUTPUT_NAME > $OUTPUT_NAME
+	else
+		ls --ignore=$script_name --ignore=$OUTPUT_NAME | sed 's/\.[a-z]*//g' > $OUTPUT_NAME
+	fi
+fi 
