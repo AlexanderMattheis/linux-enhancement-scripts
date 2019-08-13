8f7166f1dcdc4283e59bc456ff15333983708650
Alexander Mattheis <almat@gmx.de>
2019-08-13 07:30:23 +0200

    Added script to load system configuration.

---
+++ src/configuration/xbody/load_configuration.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dconf load / < system.cfg
--- src/configuration/xbody/set_gsettings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-gsettings set org.cinnamon panels-height "['1:27']"
-gsettings set org.cinnamon workspace-osd-visible false
-
-gsettings set org.cinnamon.desktop.background picture-uri "file:///usr/share/backgrounds/linuxmint-tara/proskurovskiy_palm.jpg"
-gsettings set org.cinnamon.desktop.interface icon-theme "Mint-X"
-gsettings set org.cinnamon.desktop.session idle-delay 0
-
-gsettings set org.cinnamon.theme name "Mint-X"
