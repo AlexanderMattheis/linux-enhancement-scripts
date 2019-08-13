dba0d75107130b50af24773196ad21822cd773b6
Alexander Mattheis <almat@gmx.de>
2019-08-13 21:21:47 +0200

    Revert "Added recreation of my firefox account."
    This reverts commit da2f92e45fe5afe9243352f09099835c4bdba692.

--- src/configuration/xbody/load_configurations.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dconf load / < "system.cfg"
-if [ -d "/home/oem/.mozilla" ]; then
-	sudo rm -r "/home/oem/.mozilla"  # "-r" recursive
-fi
-cp "/home/oem/System/.mozilla" "/home/oem/.mozilla"
