1a84b543fe67694de20febf20c38638652c5202e
AlexanderMattheis <almat@gmx.de>
2019-08-17 13:04:27 +0200

    Changes on configuration.

--- src/configuration/xbody/install_programs.sh
+++ src/configuration/xbody/install_programs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# regular programs------------------------------------------------------------------------------------------------------
+#regular programs------------------------------------------------------------------------------------------------------
+#install brasero
+sudo apt install -y brasero
+
 # install dconf Editor
-sudo apt install dconf-editor
+sudo apt install -y dconf-editor
 
 # install Inkscape [latest]
 sudo add-apt-repository -y ppa:inkscape.dev/stable
@@ -11,7 +14,7 @@ cp "/home/oem/Downloads/Shortcuts/inkscape.desktop" "/home/oem/Desktop/inkscape.
 
 # install IntelliJ IDEA [2019.2]
 cd "/opt/"
-sudo tar -xvzf "~/Downloads/Programs/ideaIC-2019.2.tar.gz"  # x: extract, v: verbose, z: zip/dezip (decompress), f: file
+sudo tar -xvzf "/home/oem/Downloads/Programs/ideaIC-2019.2.tar.gz"  # x: extract, v: verbose, z: zip/dezip (decompress), f: file
 cd "/home/desktop/"
 cp "/home/oem/Downloads/Shortcuts/jetbrains-idea-ce.desktop" "/home/oem/Desktop/jetbrains-idea-ce.desktop"
 cd "/home/oem/Downloads/Programs/"
@@ -21,29 +24,15 @@ sudo apt install -y "./smartgit-19_1_0.deb"
 cp "/home/oem/Downloads/Shortcuts/syntevo-smartgit.desktop" "/home/oem/Desktop/syntevo-smartgit.desktop"
 
 # install WPS Office [11.1.0.8392]"
-sudo apt install -y "./WPS Office/wps-office_11.1.0.8392_amd64.deb"
+cd "/home/oem/Downloads/Programs/WPS Office/"
+sudo apt install -y "./wps-office_11.1.0.8392_amd64.deb"
 cd "/opt/kingsoft/wps-office/office6/dicts/spellcheck/"
-sudo unzip -f "/home/oem/Downloads/Programs/WPS Office/de_DE"  # '-f' freshen existing files, without replacement
+sudo unzip "/home/oem/Downloads/Programs/WPS Office/de_DE"
 cd "/home/oem/Downloads/Programs/"
-gnome-font-viewer "/home/oem/Downloads/Programs/WPS Office/wps-fonts-master/wps/mtextra.ttf"
-gnome-font-viewer "/home/oem/Downloads/Programs/WPS Office/wps-fonts-master/wps/symbol.ttf"
-gnome-font-viewer "/home/oem/Downloads/Programs/WPS Office/wps-fonts-master/wps/WEBDINGS.TTF"
-gnome-font-viewer "/home/oem/Downloads/Programs/WPS Office/wps-fonts-master/wps/wingding.ttf"
-gnome-font-viewer "/home/oem/Downloads/Programs/WPS Office/wps-fonts-master/wps/WINGDNG2.ttf"
-gnome-font-viewer "/home/oem/Downloads/Programs/WPS Office/wps-fonts-master/wps/WINGDNG3.ttf"
+nemo "/home/oem/Downloads/Programs/WPS Office/wps-fonts-master/wps"
 cp "/home/oem/Downloads/Shortcuts/wps-office-et.desktop" "/home/oem/Desktop/wps-office-et.desktop"
 cp "/home/oem/Downloads/Shortcuts/wps-office-wpp.desktop" "/home/oem/Desktop/wps-office-wpp.desktop"
 cp "/home/oem/Downloads/Shortcuts/wps-office-wps.desktop" "/home/oem/Desktop/wps-office-wps.desktop"
-
-# snap packages---------------------------------------------------------------------------------------------------------
-# install snap package support [latest]
-sudo apt update
-sudo apt upgrade -y  # fetches the packages found by 'sudo apt-get update'
-sudo apt install -y snapd
-
-# install notepadqq [latest]
-install Notepadqq [latest]
-sudo snap install --classic notepadqq
 
 # development-----------------------------------------------------------------------------------------------------------
 # install git [latest]
@@ -55,4 +44,4 @@ sudo apt-get install -y git
 sudo apt install -y curl  # to download the official Node.js installation script
 sudo apt install -y build-essential  # to compile and install native addons
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash
-sudo apt install -y nodejs
+sudo apt install nodejs
